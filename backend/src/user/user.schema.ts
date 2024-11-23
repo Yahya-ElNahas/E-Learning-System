9c05da60c05d11a0573f7911c6750c86adad8719
Abood660 <abdelrahamanehan@gmail.com>
2024-11-23 16:35:10 +0200

    nnn

--- backend/src/user/user.schema.ts
+++ backend/src/user/user.schema.ts
@@ -2,6 +2,7 @@
 
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { Role } from './enums/role.enums';
 
 export type UserDocument = User & Document;
 
@@ -14,17 +15,31 @@ export class User {
   @Prop({ required: true, unique: true })
   email: string;
 
+
+  @Prop({
+
+    default:false
+   })
+   isVerified:boolean;
+   @Prop({
+  
+    required:false
+   })
+   verificationToken:string|null;
+
   @Prop({ required: true })
   password: string;
 
   @Prop({ required: true })
   name: string;
 
-  @Prop({ required: true, enum: ['student', 'instructor', 'admin'] })
-  role: string;
+  @Prop({ required: true, enum: Role,default:Role.Student })
+  role: Role;
 
   @Prop({ required: false })
   profile_picture_url?: string;
+
+
 
   @Prop({ default: Date.now })
   created_at: Date;
