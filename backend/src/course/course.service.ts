45d4d14276a3ce9671ec9b54d05506f828b567ba
Yahya1904 <135860509+Yahya1904@users.noreply.github.com>
2024-11-24 21:59:10 +0200

    update

--- backend/src/course/course.service.ts
+++ backend/src/course/course.service.ts
@@ -2,7 +2,7 @@ import { Injectable, NotFoundException, BadRequestException } from '@nestjs/comm
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Course, CourseDocument, Difficulty } from './course.schema';
-import { isIdValid } from 'src/user/user.service';
+import { isIdValid } from 'src/helper';
 
 @Injectable()
 export class CourseService {
--- backend/src/user/user.service.ts
+++
@@ -1,6 +0,0 @@
-import { isValidObjectId } from 'mongoose';
-import { BadRequestException } from '@nestjs/common';
-
-export function isIdValid(id: string) {
-    if(!isValidObjectId(id)) throw new BadRequestException(`Invalid ID format: ${id}`);
-}
